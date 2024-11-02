10a0f544ce08c9495a3232afe951d3939e4c82d7
yyn30056 <mong_zzo_man@naver.com>
2024-11-03 01:56:47 +0900

    fifth commit

--- more-view 파일/개고몰-팀프로젝트-복사본/js/실험2.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.popup-btn a').click(function(){
-        $('.popup-btn a').toggleClass('active')
-    })
-    $('.popup-btn').click(function(){
-        $('.popup-btn').slideToggle()
-    })
-})
